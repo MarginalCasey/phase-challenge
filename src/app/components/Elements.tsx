f1d2c54d3e9bc60bb8790d1404c2d3cf78020ad3
Casey Chang <c29429768@gmail.com>
2024-06-16 17:18:55 +0800

    Enable select element in left panel

--- src/app/components/Elements.tsx
+++ src/app/components/Elements.tsx
@@ -1,26 +1,53 @@
+import styled from "styled-components";
 import useStageStore from "../hooks/usePageStore";
 import type { IElement } from "../types";
 
+const Link = styled.div<{ $active: boolean }>`
+  color: ${(props) => (props.$active ? "red" : "white")};
+  cursor: pointer;
+`;
+
 const Elements = () => {
   const page = useStageStore((state) => state.page);
+  const activeElementPath = useStageStore((state) => state.activeElementPath);
+  const setActiveElementPath = useStageStore(
+    (state) => state.setActiveElementPath,
+  );
 
-  const renderElement = (element: IElement) => {
+  function handleClick(path: string) {
+    return () => {
+      setActiveElementPath(path);
+    };
+  }
+
+  const renderElement = (element: IElement, parentPath: string) => {
+    const path = `${parentPath}/${element.id}`;
+    const active = path === activeElementPath;
+
     if ("children" in element) {
       return (
         <div key={element.id}>
-          <div>{element.name}</div>
+          <Link $active={active} onClick={handleClick(path)}>
+            {element.name}
+          </Link>
           <div>
-            {element.children.toReversed().map((child) => renderElement(child))}
+            {element.children
+              .toReversed()
+              .map((child) => renderElement(child, path))}
           </div>
         </div>
       );
     }
 
-    return <div key={element.id}>{element.name}</div>;
+    return (
+      <Link key={element.id} $active={active} onClick={handleClick(path)}>
+        {element.name}
+      </Link>
+    );
   };
 
   return (
-    <div>{page.toReversed().map((element) => renderElement(element))}</div>
+    <div>{page.toReversed().map((element) => renderElement(element, ""))}</div>
   );
 };
 
